71740b55fc9c21194b093b3eb86093b2f325b607
lypintopimiento <lypintopimiento@gmail.com>
2021-04-12 13:17:32 -0500

    while_do_while_07_04_2021

---
+++ main.js
@@ -0,0 +1,27 @@
+
+console.log("Ciclos");
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// for (let i = ''; !(i == 'Miguel');) {
+//     i = prompt("Ingrese su nombre");
+// }
+
+// let i = 0, x = 0;
+// while(i<5 && x<5){
+//     console.log(i);
+//     console.log(`Variable constante ${x}`);
+//     i++;
+// }
+
+// let opc = '';
+// while(!(opc=="Miguel")){
+//     opc = prompt("Ingrese su nombre");
+// }
+
+// do{
+//     var opc = prompt("Ingrese su nombre");
+// }while(!(opc=="Miguel"));
